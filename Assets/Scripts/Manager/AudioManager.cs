cb13b48c3bfe9814bd3d5b45c906d266b254188e
Daniel Thalmann <daniel@thalmann.li>
2024-05-17 17:28:55 +0200

    update scene crash

---
+++ Assets/Scripts/Manager/AudioManager.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+
+public class AudioManager : MonoBehaviour
+{
+
+    
+    private EventInstance musicEventInstance;
+    private List<EventInstance> eventInstances;
+    
+
+
+    [field: Header("Music")]
+
+    [field: SerializeField]
+    public EventReference music { get; set; }
+
+    [field: Header("Player")]
+
+    [field: SerializeField]
+    public EventReference footSteps { get; set; }
+
+
+    public static AudioManager instance { get; private set; }
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogError("Found more than on Audio Manager in the scene.");
+        }
+        
+        instance = this;
+        eventInstances = new List<EventInstance>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        InitializeMusic(music);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Démarre la musique du jeu
+    /// </summary>
+    /// <param name="musicReference"></param>
+    public void InitializeMusic(EventReference musicReference)
+    {
+        musicEventInstance = CreateInstance(musicReference);
+        musicEventInstance.start();
+
+        musicEventInstance.setParameterByName("intensity", 1);
+
+    }
+
+
+    /// <summary>
+    /// Ajoute une nouvelle instance du son
+    /// </summary>
+    /// <param name="eventReference"></param>
+    /// <returns></returns>
+    public EventInstance CreateInstance(EventReference eventReference)
+    {
+        EventInstance instance =  RuntimeManager.CreateInstance(eventReference);
+        eventInstances.Add(instance);
+        return instance;
+    }
+
+
+    public void PlayOneShot(EventReference sound, Vector3 worldPos)
+    {
+        RuntimeManager.PlayOneShot(sound, worldPos);
+    }
+
+    private void CleanUp()
+    {
+        foreach(EventInstance eventInstance in eventInstances)
+        {
+            eventInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            eventInstance.release();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        CleanUp();
+    }
+
+}
--- Assets/Scripts/Navigation/AgentMoveTo.cs
+++ Assets/Scripts/Navigation/AgentMoveTo.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using FMOD.Studio;
 
 public class AgentMoveTo : MonoBehaviour
 {
@@ -9,13 +10,15 @@ public class AgentMoveTo : MonoBehaviour
     protected NavMeshAgent agent;
     protected Vector3 destination;
 
+    protected EventInstance footSteps;
+
     // Start is called before the first frame update
     void Start()
     {
         this.agent = GetComponent<NavMeshAgent>();
         destination = this.transform.position;
+        footSteps = AudioManager.instance.CreateInstance(AudioManager.instance.footSteps);
     }
-
     public void Update()
     {
         if (Input.GetMouseButtonUp(0))
@@ -31,10 +34,33 @@ public class AgentMoveTo : MonoBehaviour
         }
     }
 
+    public void FixedUpdate()
+    {
+        UpdateSound();
+    }
+
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.blue;
         Gizmos.DrawSphere(destination, .2f);
+    }
+
+    private void UpdateSound()
+    {
+        if(agent.velocity != Vector3.zero)
+        {
+            PLAYBACK_STATE playbackState;
+            footSteps.getPlaybackState(out playbackState);
+            if (playbackState.Equals(PLAYBACK_STATE.STOPPED))
+            {
+                footSteps.start();
+            }
+
+        } else
+        {
+            footSteps.stop(STOP_MODE.ALLOWFADEOUT);
+        }
     }
 
 }
